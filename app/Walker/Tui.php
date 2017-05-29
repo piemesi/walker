87cab109939e28f2de487518ee6a166bcc86b291
Malgrat <a.gajnulin@it.ecommpay.com>
2017-05-29 18:45:19 +0300

    gathered

--- app/Walker/Tui.php
+++ app/Walker/Tui.php
@@ -24,6 +24,8 @@ class Tui extends Walker implements IWalker
     const OFFER_LIST_WRAPPER = 'div#toursList',
         OFFER_LIST_ITEM = ' > article';
 
+    private $acPax = ['2AD' => 2, '1AD' => 1];
+
 
     private function getOfferItemLink()
     {
@@ -81,11 +83,59 @@ class Tui extends Walker implements IWalker
 
 //        print_r(Cache::store('memcached')->get($key));
 
-        $i     = 0;
-        $items = [];
+        $currency_pattern = '/\d|\.|\s/';
+
+        $i        = 0;
+        $items    = [];
+        $gathered = [];
         foreach ($offersWrap->getElementsByTagName('article') as $offerItem) {
             $i++;
             foreach ($offerItem->getElementsByTagName('div') as $div) {
+
+                if ($div->getAttribute('class') == 'price') {
+                    $gathered[$i]['gathered']['price']    = (int)preg_replace('/\D/', '', $div->nodeValue);
+                    $gathered[$i]['gathered']['currency'] = trim(preg_replace($currency_pattern, '', $div->nodeValue));
+                }
+
+                if ($div->getAttribute('class') == 'cityName') {
+                    $gathered[$i]['gathered']['region'] = $div->nodeValue;
+                }
+
+                if ($div->getAttribute('class') == 'tourStandart') {
+                    $gathered[$i]['gathered']['ac']  = trim($div->nodeValue);
+                    $gathered[$i]['gathered']['pax'] = $this->acPax[trim($div->nodeValue)] ?? null;
+
+                }
+
+                if ($div->getAttribute('class') == 'hotelType') {
+                    $gathered[$i]['gathered']['meal'] = trim($div->nodeValue);
+                }
+
+                if ($div->getAttribute('class') == 'tourDuration') {
+                    $gathered[$i]['gathered']['nights'] = (int)$div->nodeValue;
+                }
+
+                if ($div->getAttribute('class') == 'flightsOut') {
+                    $gathered[$i]['gathered']['start']['text'] = (int)$div->nodeValue;
+                    $gathered[$i]['gathered']['start']['wd']   = preg_replace('/(\w*)/i', '$0', $div->nodeValue);
+                    preg_match('/\d{2}.\d{2}/', $div->nodeValue, $dayText);
+
+                    $gathered[$i]['gathered']['start']['dayText'] = $dayText[0] ?? null;
+                    $dA                                           = explode('.', $gathered[$i]['gathered']['start']['dayText']);
+                    $gathered[$i]['gathered']['start']['day']     = (int)$dA[0];
+                    $gathered[$i]['gathered']['start']['month']   = (int)($dA[1] ?? null);
+                }
+
+                if ($div->getAttribute('class') == 'flightsIn') {
+                    $gathered[$i]['gathered']['finish']['text']    = (int)$div->nodeValue;
+                    $gathered[$i]['gathered']['finish']['wd']      = preg_replace('/(\w*)/i', '$0', $div->nodeValue);
+                    $gathered[$i]['gathered']['finish']['dayText'] = preg_replace('/(\d{2}\.\d{2})/i', '$0', $div->nodeValue);
+                    $dA                                            = explode('.', $gathered[$i]['gathered']['finish']['dayText']);
+                    $gathered[$i]['gathered']['finish']['day']     = (int)$dA[0];
+                    $gathered[$i]['gathered']['finish']['month']   = (int)$dA[1];
+                }
+
+
                 $items[$i][] =
                     ['classes'   => $div->getAttribute('class'),
                      'item'      => $div,
@@ -110,6 +160,16 @@ class Tui extends Walker implements IWalker
                      ]
 
                     ];
+
+                if ($div->nodeName == 'img') {
+                    $gathered[$i]['gathered']['img']['main']  = 'https://tui.ru' . $div->getAttribute('src');
+                    $gathered[$i]['gathered']['img']['x_33x'] = 'https://tui.ru' . $div->getAttribute('src');
+
+                    $x150 = str_replace('width=33', 'width=150', $div->getAttribute('src'));
+                    $x150 = str_replace('height=33', 'height=150', $x150);
+
+                    $gathered[$i]['gathered']['img']['x150'] = 'https://tui.ru' . $x150;
+                }
             }
 
             foreach ($offerItem->getElementsByTagName('a') as $div) {
@@ -124,10 +184,23 @@ class Tui extends Walker implements IWalker
 
                      ]
                     ];
+
+
+                if (empty($div->getAttribute('class'))) {
+                    $gathered[$i]['gathered']['hotelLink'] = 'https://tui.ru' . $div->getAttribute('href');
+
+                }
+
+                if ($div->getAttribute('class') == 'tourName') {
+                    $gathered[$i]['gathered']['tourName'] = trim($div->nodeValue);
+
+                    $gathered[$i]['gathered']['tourLink'] = 'https://tui.ru' . $div->getAttribute('href');
+                }
+
             }
         }
         print_r('<pre>');
-        print_r($items);
+        print_r($gathered);
         print_r('</pre>');
 
 
